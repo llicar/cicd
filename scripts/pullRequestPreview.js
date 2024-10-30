8b5e47ae80336c84ed357755749d1ae11ebb81f9
Lucas Licar <lucaslso@unicamp.br>
2024-10-30 16:51:33 -0300

    att preview 2

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -3,5 +3,8 @@ import {execSync} from "child_process";
 console.log("[DEPLOY_PREVIEW]: START")
 const command = "npm run deploy:staging";
 const output = execSync(command,{encoding: "utf8"});
-console.log(output)
+const outputLines = output.split("\n");
+const DEPLOY_URL = outputLines[outputLines.length - 1];
 console.log("[DEPLOY_PREVIEW]: END")
+
+console.log(`You can see the deploy preview on: ${DEPLOY_URL}`);
