714bf09925d8e360b4456ca167f89e01fbc70561
Lucas Licar <lucaslso@unicamp.br>
2024-10-30 16:41:56 -0300

    update preview

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -1,1 +1,7 @@
-console.log("alouuurr")
+import {execSync} from "child_process";
+
+console.log("[DEPLOY_PREVIEW]: START")
+const command = "npm run deploy:staging";
+const output = execSync(command,{encoding: "utf8"});
+console.log(output)
+console.log("[DEPLOY_PREVIEW]: END")
